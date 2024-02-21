0fb99515f5a597649bf7d505ab9034028e4f0409
CKmlo19 <luiscamilooc@gmail.com>
2024-02-21 14:20:44 -0600

    Probando el uso del teclado

--- script2048.js
+++ script2048.js
@@ -1,4 +1,17 @@
 
 function pruebaClick(id){
     alert("Click en " + id);
+
 };
+
+// funcion para los eventos del teclado
+
+function eventosTeclado(){
+    document.addEventListener('keydown', (evt) => {
+        if(evt.key == 'ArrowRight' || evt.key == 'ArrowLeft'){
+            console.log("SIp")
+        }
+    });
+}
+
+eventosTeclado();
