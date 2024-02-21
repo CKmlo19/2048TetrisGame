d55484db7d3553433d07dce28e4dfd6c7c6fc8ac
CKmlo19 <luiscamilooc@gmail.com>
2024-02-21 15:06:12 -0600

    creando el icono

--- script2048.js
+++ script2048.js
@@ -14,4 +14,15 @@ function eventosTeclado(){
     });
 }
 
+// Esta funcion crea la pieza
+function crearPieza(){
+    const nums = [2,4,8];
+    let num = Math.floor(Math.random() * 3); // esta funcion asigna un numero aleatorio entero entre 0 y 2 inclusivo
+    let elemento = document.createElement("div");
+    elemento.textContent = nums[num].toString;
+    let ubicacion = document.querySelector(".cell");
+    ubicacion.appendChild(elemento)
+
+}
+
 eventosTeclado();
