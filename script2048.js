1bd66239dc14296c88ad4e4ddcb4a499017f4947
CKmlo19 <luiscamilooc@gmail.com>
2024-02-22 14:52:49 -0600

    Ultimos cambios

--- script2048.js
+++ script2048.js
@@ -19,11 +19,24 @@ function crearPieza(){
     const nums = [2,4,8];
     let num = Math.floor(Math.random() * 3); // esta funcion asigna un numero aleatorio entero entre 0 y 2 inclusivo
     let elemento = document.createElement("div");
+    
+    elemento.classList.add("cellInterior"); //clase nueva y dentro
     elemento.textContent = nums[num];
     let ubicacion = document.querySelector(".cell0");
+    let ubicacion2 = document.querySelectorAll(".cell0");
+    console.log(ubicacion2);
+
     ubicacion.appendChild(elemento)
+}
+
+
+// game loop
+
+function game(){
+    
 
 }
+
 
 crearPieza();
 eventosTeclado();
