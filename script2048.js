2e0421b741f180742d2f82c9e4cff5e3bf2ef9b9
Joe <joquiros@estudiantec.cr>
2024-02-22 18:48:43 -0600

    Avance 1 de funcionalidades
    Agregué unas funciones pero que aun no están bien testeadas, son solo proptotipos.

--- script2048.js
+++ script2048.js
@@ -1,9 +1,44 @@
+var gameBoard = [ [0,0,0,0],
+                  [0,0,0,0],
+                  [0,0,0,0],
+                  [0,0,0,0],
+                  [0,0,0,0]]
 
-function pruebaClick(id){
-    alert("Click en " + id);
 
+
+// Intento de función para mover el bloque (solo caida)
+function dropBlock() {
+    const fallingBlock = document.getElementById('fallingBlock');
+    fallingBlock.innerText = generateRandomNumber();
 };
 
+// Función para iniciar el juego
+function startGame() {
+    setInterval(dropBlock, 1000); // Llamada a la función dropBlock cada segundo
+};
+
+// Función funciones de botones de play y pause
+function ClickActive(action){
+    if (action == 'play'){
+        generateRandomNumber();
+    } else if (action == 'pause'){
+        console.log("Juego Pausado");
+    }
+};
+
+//Funcion para generar un numero aleatorio con probabilidad truncada
+function generateRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 100);
+    if (randomNumber < 70) {
+        console.log("Número generado: 2");
+    }else if (randomNumber < 90) {
+        console.log("Número generado: 4");
+    } else if (randomNumber <= 100) {
+        console.log("Número generado: 8");
+    }else{
+        console.log("Error al crear el número aleatorio");
+    }
+};
 // funcion para los eventos del teclado
 
 function eventosTeclado(){
@@ -12,6 +47,6 @@ function eventosTeclado(){
             console.log("SIp")
         }
     });
-}
+};
 
 eventosTeclado();
